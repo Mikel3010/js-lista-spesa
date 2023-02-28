95ea20424781d4bbbfab8464b0d73011412b2c6a
Mikel3010 <mikelgremi30@gmail.com>
2023-02-28 17:19:07 +0100

    fine

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 'use strict'
+
+const listaSpesa = ['Latte', 'Prosciutto', 'Pane', 'Uova', 'Insalata', 'Acqua'];
+
+const ul = document.getElementById('list')
+
+let i = 0;
+
+while(i < 6){
+    const listItem = document.createElement('li');
+    listItem.append(listaSpesa[i]);
+    i++;
+    ul.append(listItem)
+}
